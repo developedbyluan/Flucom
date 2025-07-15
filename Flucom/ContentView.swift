6d291f8da5e5e2c632d75a7a69fcc29b1c39516b
Luan Tran <developedbyluan@gmail.com>
2025-07-15 18:15:10 +0700

    Initial Commit

---
+++ Flucom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flucom
+//
+//  Created by Tran Luan on 15/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flucom/FlucomApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlucomApp.swift
+//  Flucom
+//
+//  Created by Tran Luan on 15/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FlucomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
