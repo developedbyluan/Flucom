27f5f1010323b0b283c268590b820e0e176199f7
Luan Tran <developedbyluan@gmail.com>
2025-07-16 06:58:51 +0700

    FetchService: step 1. build fetch url

---
+++ Flucom/FetchService.swift
@@ -0,0 +1,33 @@
+//
+//  FetchService.swift
+//  Flucom
+//
+//  Created by Tran Luan on 16/7/25.
+//
+
+import Foundation
+
+struct FetchService {
+    enum FetchError: Error {
+        case badResponse
+    }
+    
+    let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api/")!
+    
+    // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
+    func fetchQuote(from show: String) async throws -> Quote {
+        // Build fetch URL
+        let quoteURL = baseURL
+            .appending(path: "quotes/random")
+        let fetchURL = quoteURL
+            .appending(queryItems: [URLQueryItem(name: "production", value: show)])
+        
+        // Fetch data
+        
+        // Handle response
+        
+        // Decode data
+        
+        // return quote
+    }
+}
