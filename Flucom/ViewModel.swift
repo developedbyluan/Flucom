f0a26312046b5b4827b0c3bbd3f7494186c68648
Luan Tran <developedbyluan@gmail.com>
2025-07-16 10:02:35 +0700

    ViewModel: @Observable, @MainActor

---
+++ Flucom/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  Flucom
+//
+//  Created by Tran Luan on 16/7/25.
+//
+
+// (theory) ViewModel: https://www.udemy.com/course/ios-15-app-development-with-swiftui-3-and-swift-5/learn/lecture/47946049?start=27#notes
+// (implementation) ViewModel: https://www.udemy.com/course/ios-15-app-development-with-swiftui-3-and-swift-5/learn/lecture/47946049?start=277#notes
+
+import Foundation
+
+@Observable
+@MainActor
+class ViewModel {
+    
+}
