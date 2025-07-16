5f492099abbdaac5a3b65f70b0ab9599e81bfdca
Luan Tran <developedbyluan@gmail.com>
2025-07-16 16:49:28 +0700

    QuoteView: render quote from samequote from init in ViewModel

--- Flucom/QuoteView.swift
+++ Flucom/QuoteView.swift
@@ -18,6 +18,10 @@ struct QuoteView: View {
                 Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
                     .resizable()
                     .frame(width: geometry.size.width * 2.7, height: geometry.size.height * 1.2)
+                
+                VStack {
+                    Text("\"\(vm.quote.quote)\"")
+                }
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
         }
