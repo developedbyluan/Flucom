2955007bac0656f2dad97b16e950757028176282
Luan Tran <developedbyluan@gmail.com>
2025-07-16 22:50:03 +0700

    QuoteView: anchor the Button to the bottom (in the same position) in 2 cases: empty view and available views

--- Flucom/QuoteView.swift
+++ Flucom/QuoteView.swift
@@ -20,47 +20,48 @@ struct QuoteView: View {
                     .frame(width: geometry.size.width * 2.7, height: geometry.size.height * 1.2)
                 
                 VStack {
-                    Spacer(minLength: 60)
-                    
-                    switch vm.status {
-                    case .notStarted:
-                        EmptyView()
-                    case .fetching:
-                        ProgressView()
-                    case .success:
-                        Text("\"\(vm.quote.quote)\"")
-                            .minimumScaleFactor(0.5)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.white) // white text even in light mode
-                            .padding()
-                            .background(.black.opacity(0.5))
-                            .clipShape(.rect(cornerRadius: 25))
-                            .padding(.horizontal)
+                    VStack {
+                        Spacer(minLength: 60)
                         
-                        ZStack(alignment: .bottom) {
-                            AsyncImage(url: vm.character.images[0]) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+                        switch vm.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text("\"\(vm.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white) // white text even in light mode
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: vm.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
+                                
+                                Text(vm.quote.character)
+                                    .foregroundStyle(.white) // white text even in light mode
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
                             }
                             .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
-                            
-                            Text(vm.quote.character)
-                                .foregroundStyle(.white) // white text even in light mode
-                                .padding(10)
-                                .frame(maxWidth: .infinity)
-                                .background(.ultraThinMaterial)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case .failed(let error):
+                            Text(error.localizedDescription)
                         }
-                        .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
-                        .clipShape(.rect(cornerRadius: 50))
-                    case .failed(let error):
-                        Text(error.localizedDescription)
+                        
+                        Spacer()
                     }
-                    
-                    
-                    Spacer()
                     
                     // Button
                     Button {
@@ -70,12 +71,12 @@ struct QuoteView: View {
                         
                     } label: {
                         Text("Get Random Quote")
-                        .font(.title)
-                        .foregroundStyle(.white)
-                        .padding()
-                        .background(.breakingBadGreen)
-                        .clipShape(.rect(cornerRadius: 7))
-                        .shadow(color: .breakingBadYellow, radius: 2)
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(.breakingBadGreen)
+                            .clipShape(.rect(cornerRadius: 7))
+                            .shadow(color: .breakingBadYellow, radius: 2)
                     }
                     
                     Spacer(minLength: 95)
