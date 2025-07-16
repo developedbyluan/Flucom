ff79394631a124dbb617a61889b89908660fcdfd
Luan Tran <developedbyluan@gmail.com>
2025-07-16 22:44:46 +0700

    QuoteView: switch + vm.status to render differen Views based on difference status

--- Flucom/QuoteView.swift
+++ Flucom/QuoteView.swift
@@ -22,33 +22,43 @@ struct QuoteView: View {
                 VStack {
                     Spacer(minLength: 60)
                     
-                    Text("\"\(vm.quote.quote)\"")
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.white) // white text even in light mode
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .clipShape(.rect(cornerRadius: 25))
-                        .padding(.horizontal)
-                    
-                    ZStack(alignment: .bottom) {
-                        AsyncImage(url: vm.character.images[0]) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            ProgressView()
+                    switch vm.status {
+                    case .notStarted:
+                        EmptyView()
+                    case .fetching:
+                        ProgressView()
+                    case .success:
+                        Text("\"\(vm.quote.quote)\"")
+                            .minimumScaleFactor(0.5)
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(.white) // white text even in light mode
+                            .padding()
+                            .background(.black.opacity(0.5))
+                            .clipShape(.rect(cornerRadius: 25))
+                            .padding(.horizontal)
+                        
+                        ZStack(alignment: .bottom) {
+                            AsyncImage(url: vm.character.images[0]) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
+                            
+                            Text(vm.quote.character)
+                                .foregroundStyle(.white) // white text even in light mode
+                                .padding(10)
+                                .frame(maxWidth: .infinity)
+                                .background(.ultraThinMaterial)
                         }
                         .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
-
-                        Text(vm.quote.character)
-                            .foregroundStyle(.white) // white text even in light mode
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
+                        .clipShape(.rect(cornerRadius: 50))
+                    case .failed(let error):
+                        Text(error.localizedDescription)
                     }
-                    .frame(width: geometry.size.width/1.1, height: geometry.size.height/1.8)
-                    .clipShape(.rect(cornerRadius: 50))
+                    
                     
                     Spacer()
                     
