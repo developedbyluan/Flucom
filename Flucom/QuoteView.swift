e76f575e3357f736474d956b315b7c85f6b82869
Luan Tran <developedbyluan@gmail.com>
2025-07-16 16:12:52 +0700

    create QuoteView bare-bone with dark color scheme for dark mode

---
+++ Flucom/QuoteView.swift
@@ -0,0 +1,19 @@
+//
+//  QuoteView.swift
+//  Flucom
+//
+//  Created by Tran Luan on 16/7/25.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+        .preferredColorScheme(.dark)
+}
